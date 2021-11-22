e21cca97a92f57c517507fca14ef9b3e94a43911
Dmitry <dimonbubyr@mail.ru>
2021-11-22 13:58:12 +0300

    Сделала карточки скриншотов

--- js/script.js
+++ js/script.js
@@ -22,4 +22,18 @@ document.addEventListener("DOMContentLoaded", function () {
         signUpModal.classList.remove('modal_active');
     };
 
+    const formSignUpButton = document.querySelector(".form__sign-up__btn");
+
+    formSignUpButton.onclick = function () {
+        signInModal.classList.remove('modal_active');
+        signUpModal.classList.add('modal_active');
+    }
+
+    const formSignInButton = document.querySelector(".form__sign-in__btn");
+
+    formSignInButton.onclick = function () {
+        signUpModal.classList.remove('modal_active');
+        signInModal.classList.add('modal_active');
+    }
+
 });
